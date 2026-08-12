8e128e4b145c1bad7118b058e96c92ed907bc134
shmily103 <supanshui@gmail.com>
2026-08-12 15:10:06 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,13 +2,14 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2026 VIKINGYFY
 
-#修改默认主题
+# 修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改lan关联IP
+# 修改 lan 关联 IP
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#添加编译日期标识
+# 添加编译日期标识
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
+# 修改默认 Wi-Fi 配置
 WIFI_FILE="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
 if [ -f "$WIFI_FILE" ]; then
     sed -i "s|set \${si}\.ssid='.*'|set \${si}\.ssid='$WRT_SSID'|g" "$WIFI_FILE"
@@ -20,30 +21,33 @@ else
     echo "错误：未找到目标文件 $WIFI_FILE"
 fi
 
+# 修改系统默认配置 (IP、主机名、香港时区)
 CFG_FILE="./package/base-files/files/bin/config_generate"
-#修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
-#修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 if [ -f "$CFG_FILE" ]; then
-    # 修改默认时区和时区显示名称
+    # 修改默认 IP 地址
+    sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" "$CFG_FILE"
+    # 修改默认主机名
+    sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" "$CFG_FILE"
+    # 修改默认时区和时区显示名称 (香港 UTC+8)
     sed -i "s|set system.@system\[0\].timezone='.*'|set system.@system[0].timezone='HKT-8'|g" "$CFG_FILE"
     sed -i "s|set system.@system\[0\].zonename='.*'|set system.@system[0].zonename='Asia/Hong_Kong'|g" "$CFG_FILE"
-    echo "默认时区修改为 Asia/Hong_Kong 成功！"
+    echo "系统默认参数 (IP/主机名/香港时区) 修改成功！"
+else
+    echo "错误：未找到目标文件 $CFG_FILE"
 fi
 
-#配置文件修改
+# 配置文件追加
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
-#手动调整的插件
+# 手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo -e "$WRT_PACKAGE" >> ./.config
 fi
 
-#无WIFI配置标志
+# 无 WIFI 配置标志
 if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 	echo "WRT_WIFI=wifi-no" >> $GITHUB_ENV
 fi
