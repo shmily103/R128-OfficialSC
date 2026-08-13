fd916251ddd9ca9a24a07c6ccfde7faa0c29a1a9
shmily103 <supanshui@gmail.com>
2026-08-14 02:40:51 +0800

    Update USB storage driver and add ethtool

--- path/filogic.mk
+++ path/filogic.mk
@@ -3676,10 +3676,10 @@ define Device/Zhao_7981R128
 DEVICE_PACKAGES := \
 		kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware \
 		-wpad-basic-mbedtls wpad-mesh-mbedtls \
-		kmod-usb2 kmod-usb3 kmod-usb-storage-extras \
-		kmod-fs-ext4 kmod-fs-vfat kmod-nls-utf8 f2fsck mkf2fs \
-		block-mount automount fdisk badblocks usbutils \
-		kmod-sfp kmod-i2c-gpio \
+		kmod-usb2 kmod-usb3 kmod-usb-storage-uas kmod-usb-storage-extras \
+		kmod-fs-ext4 kmod-fs-vfat kmod-nls-utf8 e2fsprogs \
+		block-mount fdisk usbutils \
+		kmod-sfp kmod-i2c-gpio ethtool \
 		-dnsmasq dnsmasq-full kmod-bonding kmod-vrf
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
