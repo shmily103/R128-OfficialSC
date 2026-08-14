b71d56ced135f44ae2de5e8744c41319d6d6afcc
shmily103 <supanshui@gmail.com>
2026-08-14 23:23:40 +0800

    Refactor script to simplify package setup
    Removed unnecessary directory creation and git clone commands for OpenClash and luci-app-diskman.

--- Scripts/R128-OfficialSC.sh
+++ Scripts/R128-OfficialSC.sh
@@ -3,10 +3,6 @@
 # Copyright (C) 2026 VIKINGYFY
 
 git clone https://github.com/shmily103/R128-OfficialSC.git
-mkdir package/Panzy && cd package/Panzy
-git clone --filter=blob:none --sparse --branch master https://github.com/vernesong/OpenClash.git luci-app-openclash && cd luci-app-openclash && git sparse-checkout set luci-app-openclash && cd ..
-git clone --filter=blob:none --sparse --branch master https://github.com/lisaac/luci-app-diskman.git luci-app-diskman && cd luci-app-diskman && git sparse-checkout set applications/luci-app-diskman && cd ..
-cd .. && cd ..
 cp -r R128-OfficialSC/path/01_leds target/linux/mediatek/filogic/base-files/etc/board.d
 cp -r R128-OfficialSC/path/02_network target/linux/mediatek/filogic/base-files/etc/board.d
 cp -r R128-OfficialSC/path/mediatek_filogic package/boot/uboot-tools/uboot-envtools/files
