877845a44d92026509134554bc8ff4d9443d0d9f
shmily103 <supanshui@gmail.com>
2026-08-14 15:12:42 +0800

    Change anon_mount option from '0' to '1'

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -72,7 +72,7 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 mkdir -p package/base-files/files/etc/config && cat << 'EOF' > package/base-files/files/etc/config/fstab
 config global
 	option anon_swap '0'
-	option anon_mount '0'
+	option anon_mount '1'
 	option auto_swap '1'
 	option auto_mount '1'
 	option delay_root '5'
