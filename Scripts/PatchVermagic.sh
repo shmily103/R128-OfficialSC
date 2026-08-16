5f46b5901839499e0c8b0818f1aa973c623b0fa6
shmily103 <supanshui@gmail.com>
2026-08-16 18:09:01 +0800

    Enhance PatchVermagic.sh to fetch kernel version
    Updated the script to dynamically fetch the kernel version and construct the Packages.manifest URL.

--- Scripts/PatchVermagic.sh
+++ Scripts/PatchVermagic.sh
@@ -7,7 +7,13 @@ OW_VER="25.12.5"
 
 TARGET=$(grep "^CONFIG_TARGET_BOARD=" .config | cut -d'=' -f2 | tr -d '"')
 SUBTARGET=$(grep "^CONFIG_TARGET_SUBTARGET=" .config | cut -d'=' -f2 | tr -d '"')
-URL="https://downloads.openwrt.org/releases/${OW_VER}/targets/${TARGET}/${SUBTARGET}/packages/Packages.manifest"
+
+# 1. 动态获取 kmods 目录下的内核版本（例: 6.6.x-x-xxx）
+BASE_URL="https://downloads.openwrt.org/releases/${OW_VER}/targets/${TARGET}/${SUBTARGET}/kmods"
+KMOD_VER=$(curl -sL --connect-timeout 10 "$BASE_URL/" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+-[0-9]+-[a-f0-9]+' | head -n 1)
+
+# 2. 拼接完整的 Packages.manifest 地址
+URL="${BASE_URL}/${KMOD_VER}/Packages.manifest"
 
 VERMAGIC=$(curl -sL --connect-timeout 10 "$URL" | grep -A 8 "^Package: kernel$" | grep "^Version:" | awk -F'-' '{print $NF}')
 
